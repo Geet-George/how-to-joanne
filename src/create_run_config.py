4f81631815cf0f3f4aaf9c6ac6fd2d45a8156a8d
Geet George <geet.george@mpimet.mpg.de>
2022-02-09 12:58:10 +0100

    add config file and py script to create config file
    Currently added as samples to show how config files can work for running the package

---
+++ src/create_run_config.py
@@ -0,0 +1,19 @@
+import configparser
+
+config = configparser.ConfigParser()
+
+config['DEFAULT'] = {'directory': '../extra/Sample_Data/20200122/',
+                     'aspen_suffix': '*QC.nc',
+                     'run_levels': '4',
+                     'flight_segmentation_available': False,
+                    }
+
+config['flight_segmentation'] = {}
+
+fs = config['flight_segmentation']
+fs['directory'] = ''
+
+config['quicklooks'] = {'mode': on}
+
+with open('../run_config.cfg', 'w') as configfile:
+    config.write(configfile)
